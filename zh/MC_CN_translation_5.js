6de7540c77a8fe950cc55a4353c27461eba30b02
haoyu050735 <69667026+haoyu050735@users.noreply.github.com>
2020-08-14 20:28:10 +0800

    MC_CN_translation_5(#3)
    翻譯剩餘部分+文字修正/潤飾

--- zh/MC_CN_translation_5.js
+++ zh/MC_CN_translation_5.js
@@ -1,4 +1,4 @@
-            "lis-extended-backstory":   "當地球聯邦以星際殖民發大財的計劃正起動得如火如荼之際，新星地 (Terra Nova) 是第一個因地球聯邦的重稅而感到厭倦的殖民地，他們因而開始堅決拒絕繳稅。地球聯邦大會曾嘗試以禁運令強迫新星地繳稅，但不成功；不久之後大會再以移民封鎖令向新星地施加更多壓力。\n\n幾個月後，新星地依然沒有屈服的跡象。計不成計，於是在聯邦總統的命令下派出了一支太空特戰隊，以武力方式佔領當地議會，強硬地把新星地控制權帶回聯邦。\n\n 聯邦的佔領行動激發了民憤。當時的民兵團指揮官貝福•P•托特 (Beuford P. Tots) 上校帶領平民組成反抗軍抵抗太空特戰隊的進攻，並成功將特戰隊打得落花流水，使其撤出新星地。隨即新星地殖民議會向地球聯邦大會正式宣示獨立，並將新星地重命名為獨立邦。\n\n 受到新星地事件的啟發，以及在地球聯邦暴政下的覺醒，附近星系的殖民地紛紛效法，很快地這些獨立殖民地促成了獨立國聯盟 (League of Independent States, 簡稱LIS) 的誕生 - 一個鬆散但擁有共同防禦條約對抗地球聯邦的組織。 \n\n 地球聯邦至今並不承認獨立國聯盟成員國的獨立地位，並視之為針對地球聯邦的叛匪。",
+            "lis-extended-backstory":   "當地球聯邦的星際殖民發大財的計劃正進行得如火如荼之際，新星地 (Terra Nova) 是第一個無法忍受地球聯邦的重稅的殖民地，而他們也開始堅決拒絕繳稅。地球聯邦大會曾嘗試以禁運令強迫新星地繳稅，但卻徒勞無功；不久後大會再以移民封鎖令向新星地施加更多壓力。\n\n幾個月後，新星地依然沒有屈服的跡象。計不成計，於是在聯邦總統的命令下派出了一支太空特戰隊，以武力方式佔領當地議會，強硬地把新星地控制權帶回聯邦。\n\n 聯邦的佔領行動激發了民憤。當時的民兵團指揮官貝福•P•托特 (Beuford P. Tots) 上校帶領平民組成反抗軍抵抗太空特戰隊的進攻，並成功將特戰隊打得落花流水，使其撤出新星地。隨即新星地殖民議會向地球聯邦大會正式宣示獨立，並將新星地重命名為獨立邦。\n\n 受到新星地事件的啟發，以及在地球聯邦暴政下的覺醒，附近星系的殖民地紛紛效法，很快地這些獨立殖民地促成了獨立國聯盟 (League of Independent States, 簡稱LIS) 的誕生 - 一個鬆散但擁有共同防禦條約對抗地球聯邦的組織。 \n\n 地球聯邦至今並不承認獨立國聯盟成員國的獨立地位，並視之為針對地球聯邦的叛匪。",
 
             "draconian-extended-backstory":"作為銀河系的老牌文明，艾爾法•德拉科尼亞 (Alpha Draconians) 的半蜥人 (Reptilians) 將自己比作古代先祖的後代 (古代一族 (The Ancients) 是一支已經殞落的神秘古老文明，其足跡曾經遍佈銀河系內大部份的宜居星球)。\n\n 古代一族的消失依然是一個謎，但是大批殘存科技的確由艾爾法•德拉科尼亞承存下來，給予他們巨大的實力，在與附近星系的衝突中佔盡恐怖的優勢。沒有誰比蟲族更深深瞭解這一點了，從他們在德拉科尼亞人不斷擴張的影響力下淪為次等奴隸種族的例子足以證明。\n\n 他們的科技優勢除了體現在徵服，還體現在操縱與資源開發。銀河系上不少政府皆是艾爾法•德拉科尼亞直轄，並以‘民選’出來的政府作為傀儡。這意味着，這些地方的一切金融、娛樂、教育和媒體等，都在艾爾法•德拉科尼亞宣傳機器控制之下。\n\n 艾爾法•德拉科尼亞的管治成果好壞摻半。社會精英階層靠著開發星球和奴隸暴富起來；但是高科技與半蜥人自由勞動政策的結合卻無法讓平民享受同等得益，他們反而十分貧困。民眾抗議和暴亂已是半蜥人社會上的常態；而且，基本上每當星球上的資源枯竭之時，這些不可一世的社會精英第一時間會收拾包袱讓自己離開，完全將平民沒當是一回事 - 其餘的人只能等死。 \n\n 艾爾法•德拉科尼亞要面對的問題不僅在內部，還有來自外部的威脅。索拉格帝國旗下僅有為數不多的蟲族殖民地發展出日漸成熟的科技能力與軍事策略，使得蟲族對艾爾法•德拉科尼亞的抵抗愈加激烈。",
 
@@ -28,37 +28,37 @@
 
             "manual-creative-mode":     "對於只想放鬆和建造的玩家來說，創造模式無非是最好的選擇。當以創造模式進行遊戲時，遊戲中的所有結構都是解鎖的，且任何技術或資源即可建造。當然，處於創意模式的遊戲無法切換到線上模式。",
 
-            "manual-single-city":       "Whether you start a game in local, online, or creative mode, you are able to choose between a single city and a regional game.\n\nIn a single city game, your entire city is contained within one map and one file.  Upon starting the game, you are able to select what size of map you wish to play on, as well as the abundance of starting resources on that map.  Later on in the game as your government level increases, you are able to expand your map size by annexing new lands.  The downfall of this is that game performance degrades as the size of your map increases, particularly on slower devices.  For this reason, the Region mode was created.",
+            "manual-single-city":       "無論您是以本地，線上還是創造模式啟動遊戲，都可以在單一城市和區域模式之間進行選擇。\n\n 在單一城市中，整個城市都包含在一張地圖和一個文件中。 開始遊戲後，您可以選擇地圖大小，以及資源的豐富度。 在遊戲的後期，隨著政府級別的提高，您可以通過吞併新土地來擴張地圖。 然而，隨著地圖尺寸的增加，遊戲性能亦會下降，尤其是在性能較差的設備上。 為解決這個問題，“區域模式”於是誕生。",
 
-            "manual-regions":           "Regional mode was added to the game due to performance issues related to ever expanding single city maps.  A region caps map sizes at either the small or medium level, and the ability to expand maps using the annexation feature has been removed.  The trade off to this limitation is the fact that a single region may contain up to 2,500 individual map files in total, theoretically allowing for massive colonies.\n\nRegions work by splitting a city up into smaller chunks, with each chuck being processed separately, yet sharing a common resource and technology pool.  This setup increases the performance of ultra large cities dramatically, and allows a city to be built in specialized chunks.  Economic and population statistics are available for both the individual cities and the region as a whole, but for online reporting purposes, the stats for the entire region are used.",
+            "manual-regions":           "區域模式的誕生是為了解決單個城市擴張後衍生的裝置效能問題。區域將地圖大小限制在小型或中型級別，並停用了藉由吞併方式擴展地圖的功能。這個限制帶來的好處是，單個區域至多可包含2500個單獨的地圖文件，理論上可以容納大量的殖民地。\n\n 區域模式的運作原理是將原本的城市資訊分成許多小的數據包，每個數據包都單獨處理，但共享一個公共資源和技術庫。此設置大大地提高了大型城市的運作效能，並允許將城市構建為專用塊。  經濟和人口等統計資訊方，無論是區域中的單一城市或整個區域都有詳盡的紀錄。但為了方便在線上公告，系統將採用整個區域的相關統計資料。",
 
             "gen-map-types":            "星球種類",
 
-            "manual-map-types":         "The type of world you choose to play on will have a large impact on the way you play and the difficulty of the game.  Review the details of each map type carefully before starting a new colony.",
+            "manual-map-types":         "您所選擇的世界類型將影響您的遊戲體驗和遊戲難度。在建立新殖民地之前，請仔細檢查每張地圖的相關資訊。",
 
             "gen-races":                "種族",
 
-            "manual-races-desc":        "In My Colony, there are several principal species, each with distinctive starting bonuses.  The civilization you choose will determine the species of your initial colonists, but once your city grows in size and reach, other species from across the galaxy may well join your colony.",
+            "manual-races-desc":        "在My Colony中有幾個主要物種，每個物種都有獨特的能力。 您選擇的文明將決定您初始殖民者的物種。然而，隨著您的城市規模擴大，銀河系中的其他物種也可能會加入您的殖民地。",
 
-            "manual-play-modes-desc":   "There are several way to play My Colony, offering variety and countless hours of playtime.  You can play online, offline, creatively, with a custom map, with a single city, or on a large regional landscape.  The game allows for countless save files, so you can try all styles to see which you like best!",
+            "manual-play-modes-desc":   "My Colony提供了各種模式來讓您消磨時間。 您可以線上、離線遊玩，甚至自訂地圖，無論是以單一城市模式或區域模式。本遊戲可以讓您保存無數個存檔，因此您可以自由探索，直到發現最適合您的遊戲方式！",
 
-            "manual-res-desc":          "Resource management is a huge core component of My Colony, and proper handling of resource inflows, outflows, and storage is vital if you want your city to grow and prosper.  Resources allow you to build your colony, grow your population, and research new technologies.",
+            "manual-res-desc":          "資源管理在My Colony中有舉足輕重的地位。如果您希望城市能夠繁榮發展，物流管理和存儲將會非常重要。 資源使您可以建立自己的殖民地、增加人口並研發新技術。",
 
-            "manual-tech-header":       "The research and discovery of technology allows you to unlock new buildings and vehicles, thus opening up new ways to grow your colony.\n\nThe tech hierarchy in My Colony is not linear.  For example, if you were trying to draw out the tech tree in a structured programmatic way (for this manual, for example), it would not be possible or practical, as each new technology is dependent on technologies from not only several different branches in the tree, but also on resources which may or may not have been unlocked by the player at any given time.  Therefore, in order to be easily readable, any visual tech tree would need to be manually created and updated by hand.  Such tech trees exist on the My Colony online reference website here: https://www.my-colony.com/reference/\n\nFor the purposes of this manual, all technologies are here listed in alphabetical order for easy lookup and reference, along with a description of each, their requirements, and what they may unlock.",
+            "manual-tech-header":       "研發新技術將能讓您解鎖新的建築物和車輛，從而開啟殖民地發展的嶄新篇章。\n\n 在My Colony中，技術間並非線性關係。若您想要跟這本手冊一樣以結構化方式來製作科技樹的話，您將會遇到很大的障礙，因為每種新技術都不僅依賴於來自多個不同分支機構的技術。每一項新技術不僅與樹中其他的技術有交互關聯，同時也與玩家現有掌握的資源有關。 因此，為了易於閱讀，科技樹的任何變動都需要手動創建和更新。 您可以在 https://www.my-colony.com/reference/ 檢視這顆科技樹。\n\n 本手冊以字母順序列出了所有的技術，以便於查找和參考，並分別說明每種技術的研發要求以及可解鎖的內容。",
 
-            "manual-buildings-desc":    "There are a lot of buildings available in My Colony, #gamebuildingcount in total.  For this reason, placing them in any order other than alphabetical in this manual would make lookup and reference extremely difficult.\n\nThe online My Colony reference at https://www.my-colony.com/reference/ provides interactive lookup tables for all structures in the game, including tech requirements, costs, and production output values.",
+            "manual-buildings-desc":    "My Colony中有多達#gamebuildingcount以上的建築物。 因此，若不以字母順序排序這些項目的話，將會使查找資料的難度大增\n\n你可以透過 https://www.my-colony.com/reference/的互動式表格搜尋遊戲中的所有結構，包括其所需技術要求，建造成本和生產產值。",
 
             "enc-print-status":         "警告！您將列印的是接近200頁的東西！",
 
-            "manual-vehicles-desc":     "Vehicles (or Units) are the primary workhorses in My Colony.  They allow you to build structures and harvest vital resources.  There are many different units for you to build and manage in the game.  Here they are listed in alphabetical order for your reference.",
+            "manual-vehicles-desc":     "車輛（或單位）是殖民地發展的主力。 它們可以建造結構並收穫重要資源。 您可以在遊戲中建造和管理許多不同的單元。 在這裡，它們按字母順序列出以供您參考。",
 
             "manual-terms-header":      "關鍵字附錄",
 
-            "manual-terms-desc":        "Throughout this manual, and within the game itself, there are several terms that you should be familiar with in order to fully understand what is going on.",
+            "manual-terms-desc":        "在本手冊中及遊戲中，您應該熟悉幾個術語，以便了解情況。",
 
             "manual-concepts-header":   "基礎教程",
 
-            "manual-concepts-desc":     "The section describes some of the key gameplay concepts in My Colony, including how to get up and running with your first colony and how to perform some of the basic tasks in the game.",
+            "manual-concepts-desc":     "本段介紹了My Colony中的一些重要概念，包括如何啟動並運行您的第一個殖民地，以及如何執行遊戲中的一些基本任務。",
 
             "waterworld-base-tech":     "在海洋星球用來研究的基地",
 
@@ -294,7 +294,7 @@
 
             "merch-store-header":       "My Colony 精品商店",
 
-            "support-community-desc":   "網上論壇社群",
+            "support-community-desc":   "線上論壇社群",
 
             "merch-store-desc":         "Buy My Colony Swag!",
 
